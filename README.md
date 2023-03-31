dae4ed2e5d4ee93a7a95eee8fbab41e958b53150
Adrian Ariton <adrianariton@192.168.1.7>
2023-03-31 21:48:06 +0300

    Fixed allf

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Testing some possible function implementation
 in [para.cpp](para.cpp). This is only for testing purposes
-in the event that i get accepted(as the final code will look much nicer :) )
+in the event that i get accepted( as the final code will look much nicer :) )
--- para.cpp
+++ para.cpp
@@ -6,6 +6,9 @@
 #include <taskflow/algorithm/reduce.hpp>
 #include <taskflow/core/task.hpp>
 
+/***
+ * init should be 0 for expected results
+*/
 template <typename B, typename E, typename T, typename O>
 tf::Task one_of(tf::Taskflow& taskflow, B beg, E end, T& init, O bop) {
     auto reduce_bop = [&] (int l, int r) mutable throw() -> int
@@ -36,30 +39,17 @@ tf::Task minf_of(tf::Taskflow& taskflow, B beg, E end, T& init, O bop) {
     return taskflow.reduce(beg, end, init, reduce_bop);
 }
 
+
+/***
+ * init should be 1 for expected results
+*/
 template <typename B, typename E, typename T, typename O>
 tf::Task allf_of(tf::Taskflow& taskflow, B beg, E end, T& init, O bop) {
 
     // o workey, no goody :( will doodey tommorowey
     auto reduce_bop = [&] (int l, int r) mutable throw() -> int
     { 
-        auto [init_, cond, yes, no] = taskflow.emplace(
-            [=] () { return bop(l) && bop(r);},
-            [=] () { return 0; },
-            [] () { std::cout << "yes\n"; },
-            [] () { std::cout << "no\n"; }
-        );
-
-        init_.name("init");
-        cond.name("cond");
-        yes.name("yes");
-        no.name("no");
-
-        cond.succeed(init);
-
-        // With this order, when cond returns 0, execution
-        // moves on to yes. When cond returns 1, execution
-        // moves on to no.
-        cond.precede(yes, no);
+        return bop(l) && bop(r);
     };
     return taskflow.reduce(beg, end, init, reduce_bop);
 }
@@ -67,15 +57,15 @@ int main(){
   
     tf::Executor executor;
     tf::Taskflow taskflow;
-    int sum = 0;
+    int sum = 1;
     std::vector<int> vec;
     std::vector<int>::iterator first, last;
 
     tf::Task init = taskflow.emplace([&](){
-        vec   = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        vec   = {1,3,5,2,1,7};
+        vec   = {1, 2, 5, 7};
         first = vec.begin();
         last  = vec.end();
+        sum = 1;
     });
 
     tf::Task task = allf_of(taskflow, std::ref(first), std::ref(last), sum, 
@@ -91,7 +81,5 @@ int main(){
     init.precede(task);
 
     executor.run(taskflow).wait();
-
-    printf("%d", sum);
   return 0;
 }
